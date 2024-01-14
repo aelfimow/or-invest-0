2d67eaeeb5b91ea803a5aaad720e448f01895cc0
Alexander Elfimow <my.free.mail@gmx.de>
2024-01-14 12:26:46 +0100

    Added class

--- src/main.cpp
+++ src/main.cpp
@@ -6,6 +6,56 @@
 using fp64 = double;
 static_assert(sizeof(fp64) == 8U);
 
+class Investment
+{
+    public:
+        Investment() :
+            m_price { 0.0 },
+            m_dividend { 0.0 },
+            m_count { 0 }
+        {
+        }
+
+        ~Investment()
+        {
+        }
+
+        void price(fp64 value)
+        {
+            m_price = value;
+        }
+
+        fp64 price() const
+        {
+            return m_price;
+        }
+
+        void dividend(fp64 value)
+        {
+            m_dividend = value;
+        }
+
+        fp64 dividend() const
+        {
+            return m_dividend;
+        }
+
+        void count(size_t count)
+        {
+            m_count = count;
+        }
+
+        size_t count() const
+        {
+            return m_count;
+        }
+
+    private:
+        fp64 m_price;
+        fp64 m_dividend;
+        size_t m_count;
+};
+
 int main(int, char *[])
 {
     fp64 const I_max = 10.0E3;
